e840976c73ec08f0bdb3cb7bb24bb7e29a6b85c7
nreis11 <nicholasreis@icloud.com>
2023-04-14 17:43:52 -0700

    add scheduler

--- config/initializers/heroku_keep_alive.rb
+++ config/initializers/heroku_keep_alive.rb
@@ -2,9 +2,11 @@ require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.new
 
 if Rails.env.production?
-  scheduler.cron '0 9-21 * * *' do
+  scheduler.cron '*/15 9-21 * * *' do
+     url = "https://res2go.onrender.com"
+     puts "Pinging #{url}"
      require "net/http"
      require "uri"
-     Net::HTTP.get_response(URI.parse(ENV["HOSTNAME"]))
+     Net::HTTP.get_response(URI.parse(url))
   end
 end
